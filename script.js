0489d435ab372d88f4c5e59924c4b2640bf14459
AWD-2069 <armstrongwebdev@gmail.com>
2025-01-07 15:37:39 -0500

    Update script.js
    Add Carousel code
    
    Signed-off-by: AWD-2069 <armstrongwebdev@gmail.com>

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ menuLinks.forEach(link => {
       mobileMenu.classList.add('hidden');
   });
 });
+
+const carousel = document.getElementById('carousel');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentIndex = 0;
+const totalImages = carousel.children.length;
+
+function updateCarousel() {
+    const offset = -currentIndex * carousel.children[0].clientWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
+}
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarousel();
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarousel();
+}
+
+// Automatic sliding every 3 seconds
+setInterval(showNextImage, 3000);
+
+// Add event listeners for buttons
+nextButton.addEventListener('click', showNextImage);
+prevButton.addEventListener('click', showPrevImage);
