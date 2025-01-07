022127f232021cb435e9c35a2dd1ff731015377d
AWD-2069 <armstrongwebdev@gmail.com>
2025-01-07 16:25:50 -0500

    Update script.js
    Signed-off-by: AWD-2069 <armstrongwebdev@gmail.com>

--- script.js
+++ script.js
@@ -16,34 +16,3 @@ menuLinks.forEach(link => {
       mobileMenu.classList.add('hidden');
   });
 });
-
-function carousel() {
-  return {
-    currentIndex: 0, // Track the index of the current image
-    images: [
-      'https://via.placeholder.com/600x300?text=Slide+1',
-      'https://via.placeholder.com/600x300?text=Slide+2',
-      'https://via.placeholder.com/600x300?text=Slide+3',
-    ],
-    interval: null,
-    startAutoPlay() {
-        this.interval = setInterval(() => {
-            this.next();
-        }, 3000); // Change every 3 seconds
-    },
-    stopAutoPlay() {
-        clearInterval(this.interval);
-    },
-    // Method to go to the next image
-    next() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    },
-    // Method to go to the previous image
-    prev() {
-      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    },
-    init() {
-        this.startAutoPlay();
-    }
-  };
-}
